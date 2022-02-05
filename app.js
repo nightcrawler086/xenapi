06ae999afe7d82547015073188f3235acbbdab69
Brian Hall <brian.hall4@gmail.com>
2022-02-05 16:11:34 -0500

    adding moran for some logging

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
+var morgarn = require('morgan')
+
+app.use(morgan('short'))
 
 app.get('/', (req, res) => {
     res.send('Hello Other World!')
